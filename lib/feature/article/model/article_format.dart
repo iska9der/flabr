2d216e2686c77a2c55412fc56b4c6f1ee1b8b705
iska9der <iskander94y@gmail.com>
2023-10-10 02:39:24 +0300

    удален избыточный вариант

--- lib/feature/article/model/article_format.dart
+++ lib/feature/article/model/article_format.dart
@@ -52,6 +52,5 @@ enum ArticleFormat {
         faq || roadmap || tutorial => const Color(0xffd5700b),
         interview || reportage => const Color(0xff1da53d).withOpacity(.7),
         analytics || retrospective => const Color(0xffc23d96),
-        _ => const Color(0xff9d9d9d),
       };
 }
