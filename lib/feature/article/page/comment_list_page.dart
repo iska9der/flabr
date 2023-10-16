7d8be79f326b94924a636a516a0847d8016723a3
iska9der <iskander94y@gmail.com>
2023-10-17 01:28:52 +0300

    комментарии: рефакторинг, добавлен футер
    - улучшена производительность комментариев с большой вложенностью,
    но все еще далеко от идеала

--- lib/feature/article/page/comment_list_page.dart
+++ lib/feature/article/page/comment_list_page.dart
@@ -5,22 +5,21 @@ import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart
 import 'package:intl/intl.dart';
 
 import '../../../common/model/extension/state_status_x.dart';
-import '../../../common/model/stat_type.dart';
 import '../../../component/di/dependencies.dart';
 import '../../../config/constants.dart';
 import '../../../widget/app_expansion_panel.dart';
 import '../../../widget/html_view_widget.dart';
 import '../../../widget/progress_indicator.dart';
-import '../../../widget/stat_text_widget.dart';
 import '../../settings/cubit/settings_cubit.dart';
 import '../cubit/comment_hidden_cubit.dart';
 import '../cubit/comment_list_cubit.dart';
 import '../model/comment_model.dart';
 import '../repository/article_repository.dart';
 import '../widget/article_author_widget.dart';
+import '../widget/comment/comment_rating_widget.dart';
 
-const paddingBetweenTrees = 44.0;
-const paddingBetweenChilds = 22.0;
+const paddingBetweenTrees = 12.0;
+const paddingBetweenChilds = 4.0;
 
 @RoutePage(name: CommentListPage.routeName)
 class CommentListPage extends StatelessWidget {
@@ -69,7 +68,6 @@ class CommentListView extends StatelessWidget {
           builder: (context, state) {
             if (state.status.isInitial) {
               context.read<CommentListCubit>().fetch();
-
               return const CircleIndicator();
             }
 
@@ -82,7 +80,6 @@ class CommentListView extends StatelessWidget {
             }
 
             final comments = state.list.comments;
-
             if (comments.isEmpty) {
               return const Center(
                 child: Text('Нет комментариев'),
@@ -91,23 +88,14 @@ class CommentListView extends StatelessWidget {
 
             return ListView.separated(
               itemCount: comments.length,
-              padding: const EdgeInsets.symmetric(
-                horizontal: kScreenHPadding,
-                vertical: kScreenHPadding * 0.5,
-              ),
-              separatorBuilder: (c, i) => const Divider(
+              padding: const EdgeInsets.fromLTRB(4, 4, 4, 16),
+              separatorBuilder: (c, i) => const SizedBox(
                 height: paddingBetweenTrees,
               ),
               itemBuilder: (context, index) {
                 final comment = comments[index];
-                bool isLast = index + 1 == comments.length;
 
-                return Padding(
-                  padding: EdgeInsets.only(
-                    bottom: isLast ? paddingBetweenTrees : 0,
-                  ),
-                  child: CommentTreeWidget(comment),
-                );
+                return CommentTreeWidget(comment);
               },
             );
           },
@@ -125,6 +113,10 @@ class CommentTreeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authorColor = comment.isPostAuthor
+        ? Colors.yellowAccent.withOpacity(.12)
+        : Theme.of(context).colorScheme.surface;
+
     return BlocBuilder<CommentHiddenCubit, CommentHiddenState>(
       builder: (context, state) {
         return AppExpansionPanelList(
@@ -140,72 +132,47 @@ class CommentTreeWidget extends StatelessWidget {
               isExpanded: !state.hiddenComments.contains(comment.id),
               backgroundColor: Theme.of(context).scaffoldBackgroundColor,
               iconBuilder: (child, isExpanded) {
-                return ClipRRect(
-                  clipBehavior: Clip.hardEdge,
-                  borderRadius: const BorderRadius.only(
-                    topRight: Radius.circular(kBorderRadiusDefault),
-                    bottomRight: Radius.circular(kBorderRadiusDefault),
-                  ),
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: comment.isPostAuthor
-                          ? Colors.yellowAccent.withOpacity(.12)
-                          : Theme.of(context).colorScheme.surface,
+                return DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: authorColor,
+                    borderRadius: BorderRadius.only(
+                      topRight: const Radius.circular(kBorderRadiusDefault),
+                      bottomRight: !isExpanded
+                          ? const Radius.circular(kBorderRadiusDefault)
+                          : Radius.zero,
                     ),
-                    child: child,
                   ),
+                  child: child,
                 );
               },
               headerBuilder: (context, isExpanded) {
-                return ClipRRect(
-                  clipBehavior: Clip.hardEdge,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(kBorderRadiusDefault),
-                    bottomLeft: Radius.circular(kBorderRadiusDefault),
-                  ),
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: comment.isPostAuthor
-                          ? Colors.yellowAccent.withOpacity(.12)
-                          : Theme.of(context).colorScheme.surface,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: Row(
-                        children: [
-                          /// Автор
-                          ArticleAuthorWidget(comment.author),
-
-                          /// Заполняем пространство между виджетами
-                          const Expanded(child: Wrap()),
-
-                          /// Очки
-                          StatTextWidget(
-                            type: StatType.score,
-                            value: comment.score,
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-
-                          const SizedBox(width: 12),
-                        ],
-                      ),
+                return DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: authorColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: const Radius.circular(kBorderRadiusDefault),
+                      bottomLeft: !isExpanded
+                          ? const Radius.circular(kBorderRadiusDefault)
+                          : Radius.zero,
                     ),
                   ),
+                  child: ArticleAuthorWidget(comment.author),
                 );
               },
               body: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CommentWidget(comment),
-                  ),
-                  for (var child in comment.children)
-                    Padding(
+                  CommentWidget(comment),
+                  ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: comment.children.length,
+                    itemBuilder: (_, i) => Padding(
                       padding: const EdgeInsets.only(top: paddingBetweenChilds),
-                      child: CommentTreeWidget(child),
-                    )
+                      child: CommentTreeWidget(comment.children[i]),
+                    ),
+                  )
                 ],
               ),
             ),
@@ -223,33 +190,48 @@ class CommentWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const textPadding = 4.0;
-    final paddingLeft = 6.0 * comment.level;
+    const basePadding = 10.0;
+    final additional = comment.level == 0 ? 0 : comment.level + 6;
+    final paddingLeft = basePadding + additional;
 
-    return Padding(
-      padding: EdgeInsets.only(left: paddingLeft),
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: const BorderRadius.only(
+          bottomRight: Radius.circular(kBorderRadiusDefault),
+          bottomLeft: Radius.circular(kBorderRadiusDefault),
+        ),
+      ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          /// Дата коммента
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: textPadding,
-              vertical: textPadding * 1.5,
-            ),
-            child: Text(
-              DateFormat.yMd().add_jm().format(comment.publishedAt),
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          ),
-
           /// Текст
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: textPadding),
+            padding: EdgeInsets.only(left: paddingLeft, right: basePadding),
             child: HtmlView(
               textHtml: comment.message,
               renderMode: RenderMode.column,
               padding: EdgeInsets.zero,
+            ),
+          ),
+
+          /// Футер
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: basePadding,
+              vertical: basePadding,
+            ),
+            child: Row(
+              children: [
+                CommentRatingWidget(comment),
+                const Spacer(),
+                Text(
+                  DateFormat.yMd().add_jm().format(comment.publishedAt),
+                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
+              ],
             ),
           ),
         ],
