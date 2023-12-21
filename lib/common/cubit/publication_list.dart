4a2705cc187260608c649caf4f68d514522f6ead
iska9der <iskander94y@gmail.com>
2023-12-22 02:24:12 +0300

    дженерик кубит для списка публикаций

---
+++ lib/common/cubit/publication_list.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../feature/publication/model/publication/publication.dart';
+
+enum PublicationListStatus { initial, loading, success, failure }
+
+abstract class PublicationListC<State extends PublicationListS>
+    extends Cubit<State> {
+  PublicationListC(super.initialState);
+
+  bool get isFirstFetch;
+
+  /// Получение списка публикаций
+  FutureOr<void> fetch();
+}
+
+abstract interface class PublicationListS {
+  PublicationListStatus get status;
+  String get error;
+  int get page;
+  int get pagesCount;
+  List<Publication> get publications;
+}
