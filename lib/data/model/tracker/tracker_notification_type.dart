e8572a200ef1ca92ce1ef7b4bcb5d572776818d4
iska9der <iskander94y@gmail.com>
2024-12-24 00:23:20 +0300

    новый тип уведомления: thread_add

--- lib/data/model/tracker/tracker_notification_type.dart
+++ lib/data/model/tracker/tracker_notification_type.dart
@@ -4,19 +4,23 @@ enum TrackerNotificationType {
   unknown,
   postAdd,
   postAddToFavorite,
+  threadAdd,
   threadAddToFavorite,
   ;
 
   factory TrackerNotificationType.fromString(String value) => switch (value) {
         'post_add' => TrackerNotificationType.postAdd,
         'post_add_to_favorite' => TrackerNotificationType.postAddToFavorite,
+        'thread_add' => TrackerNotificationType.threadAdd,
         'thread_add_to_favorite' => TrackerNotificationType.threadAddToFavorite,
         _ => TrackerNotificationType.unknown,
       };
 
   String get text => switch (this) {
         TrackerNotificationType.unknown => 'Что-то произошло с',
-        TrackerNotificationType.postAdd => 'Опубликовал новую публикацию',
+        TrackerNotificationType.postAdd ||
+        TrackerNotificationType.threadAdd =>
+          'Опубликовал новую публикацию',
         TrackerNotificationType.postAddToFavorite ||
         TrackerNotificationType.threadAddToFavorite =>
           'Добавил в закладки новую публикацию',
