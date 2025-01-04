830bea31375bb5356b1f656e6ed49ecd9376cfd0
iska9der <iskander94y@gmail.com>
2025-01-05 01:39:20 +0300

    подготовка для релиза на f-droid

--- builder.sh
+++ builder.sh
@@ -2,4 +2,4 @@ flutter pub get
 
 flutter pub run build_runner build --delete-conflicting-outputs
 
-flutter build apk --release --split-per-abi
+flutter build apk --release --split-per-abi --dart-define-from-file env/variables.json
