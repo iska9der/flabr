95afdf4eb39d1c8243f8edb601a0e3046f19a9d4
iska9der <iskander94y@gmail.com>
2025-03-27 00:06:12 +0300

    android: форсирование высокой частоты обновления экрана

--- lib/core/helper/app_initializer.dart
+++ lib/core/helper/app_initializer.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 
@@ -11,11 +14,19 @@ import '../constants/constants.dart';
 abstract class AppInitializer {
   static Future<void> init() async {
     await configureDependencies(env: AppEnvironment.env);
-    await AppInitializer.loadIntl();
-    AppInitializer.loadLicense();
+
+    await loadIntl();
+
+    loadLicense();
 
     if (kDebugMode) {
       Bloc.observer = MyBlocObserver();
+    }
+
+    if (Platform.isAndroid) {
+      /// максимальная герцовка
+      /// issue https://github.com/flutter/flutter/issues/35162
+      await FlutterDisplayMode.setHighRefreshRate();
     }
   }
 
@@ -24,12 +35,8 @@ abstract class AppInitializer {
     await initializeDateFormatting(defaultLocale);
   }
 
-  static void loadLicense() {
-    LicenseRegistry.addLicense(() async* {
-      final geologica = await rootBundle.loadString(
-        LicenseAssets.fontGeologica,
-      );
-      yield LicenseEntryWithLineBreaks(['flabr'], geologica);
-    });
-  }
+  static void loadLicense() => LicenseRegistry.addLicense(() async* {
+    final geologica = await rootBundle.loadString(LicenseAssets.fontGeologica);
+    yield LicenseEntryWithLineBreaks(['flabr'], geologica);
+  });
 }
